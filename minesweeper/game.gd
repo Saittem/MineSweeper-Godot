44a0cb31c696d510cacd4f9dd6687d14a474f16d
Saittem <TEOLF@email.cz>
2025-07-05 00:54:55 +0200

    Base game finished

--- minesweeper/game.gd
+++ minesweeper/game.gd
@@ -9,37 +9,41 @@ var remaining_mines : int
 @onready var HUD = $HUD
 @onready var GameOver = $GameOver
 
-func _ready():
+func _ready() -> void:
 	new_game()
 
-func new_game():
+func new_game() -> void:
 	time_elapsed = 0
 	remaining_mines = total_mines
+	$TileMap.new_game()
 	GameOver.hide()
 	get_tree().paused = false
 
-func _process(delta: float):
+func _process(delta: float) -> void:
 	time_elapsed += delta
 	HUD.get_node("stopwatchLabel").text = str(int(time_elapsed))
 	HUD.get_node("flagsLabel").text = str(remaining_mines)
-	
-	if remaining_mines == 0:
-		game_won()
 
-func _on_tile_map_flag_placed():
+func _on_tile_map_flag_placed() -> void:
 	remaining_mines -= 1
 
-func _on_tile_map_flag_removed():
+func _on_tile_map_flag_removed() -> void:
 	remaining_mines += 1
 
-func end_game():
+func end_game() -> void:
 	GameOver.show()
 	get_tree().paused = true
 
-func game_won():
+func game_won() -> void:
 	end_game()
 	GameOver.get_node("Panel/Label").text = "Won"
 
-func _on_tile_map_game_lost():
+func _on_tile_map_game_lost() -> void:
 	end_game()
 	GameOver.get_node("Panel/Label").text = "Lost"
+
+func _on_game_over_restart() -> void:
+	new_game()
+
+func _on_tile_map_game_won() -> void:
+	game_won()
--- minesweeper/tile_map.gd
+++ minesweeper/tile_map.gd
@@ -4,6 +4,7 @@ extends Node
 signal flag_placed
 signal flag_removed
 signal game_lost
+signal game_won
 
 #grid consts
 const Y_OFFSET : int = 2
@@ -21,13 +22,6 @@ var tile_id : int = 0
 @onready var numbers_tilemaplayer = $Numbers
 @onready var mines_tilemaplayer = $Mines
 
-#layer variables
-#var mine_layer : int = -4
-#var number_layer : int = -3
-#var grass_layer : int = -2
-#var flag_layer : int = -1
-#var hover_layer : int = 0
-
 #atlas tile coords
 var mine_atlas := Vector2i(4, 0)
 var number_atlas : Array = generate_number_atlas()
@@ -37,16 +31,16 @@ var flag_atlas := Vector2i(5, 0)
 #array to store mine coords
 var mine_coords := []
 
-func generate_number_atlas():
+func generate_number_atlas() -> Array:
 	var a := []
 	for i in range(8):
 		a.append(Vector2i(i, 1))
 	return a
 
-func _ready():
+func _ready() -> void:
 	new_game()
 
-func _input(event):
+func _input(event) -> void:
 	if event is InputEventMouseButton:
 		#checks if the mouse is on the game board
 		if (event.position.y < ROWS * CELL_SIZE) and (event.position.y >= 2 * CELL_SIZE):
@@ -61,7 +55,7 @@ func _input(event):
 			elif event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 				process_right_click(map_pos)
 
-func process_left_click(pos):
+func process_left_click(pos) -> void:
 	var revealed_cells := []
 	var cells_to_reveal := [pos]
 	while not cells_to_reveal.is_empty():
@@ -73,8 +67,14 @@ func process_left_click(pos):
 		if not is_number(cells_to_reveal[0]):
 			cells_to_reveal = reveal_surrounding_cells(cells_to_reveal, revealed_cells)
 		cells_to_reveal.erase(cells_to_reveal[0])
+	var all_cleared := true
+	for cell in numbers_tilemaplayer.get_used_cells():
+		if is_grass(cell):
+			all_cleared = false
+	if all_cleared:
+		game_won.emit()
 
-func process_right_click(pos):
+func process_right_click(pos) -> void:
 	#checks if it is a grass cell
 	if is_grass(pos):
 		if is_flag(pos):
@@ -84,7 +84,7 @@ func process_right_click(pos):
 			flags_tilemaplayer.set_cell(pos, tile_id, flag_atlas)
 			flag_placed.emit()
 
-func reveal_surrounding_cells(cells_to_reveal, revealed_cells):
+func reveal_surrounding_cells(cells_to_reveal, revealed_cells) -> Array:
 	for i in get_all_surround_cells(cells_to_reveal[0]):
 		# skip if already revealed or mine
 		if not revealed_cells.has(i) and not is_mine(i):
@@ -92,31 +92,31 @@ func reveal_surrounding_cells(cells_to_reveal, revealed_cells):
 				cells_to_reveal.append(i)
 	return cells_to_reveal
 
-func _process(_delta):
+func _process(_delta) -> void:
 	highlight_cell()
 
-func highlight_cell():
+func highlight_cell() -> void:
 	var mouse_pos : Vector2i = hover_tilemaplayer.local_to_map(hover_tilemaplayer.get_local_mouse_position())
 	hover_tilemaplayer.clear()
 	#hover over grass
 	if is_grass(mouse_pos):
 		hover_tilemaplayer.set_cell(mouse_pos, tile_id, hover_atlas)
 
-func new_game():
+func new_game() -> void:
 	tilemap_clear()
 	mine_coords.clear()
 	generate_mines()
 	generate_numbers()
 	generate_grass()
 
-func tilemap_clear():
+func tilemap_clear() -> void:
 	hover_tilemaplayer.clear()
 	flags_tilemaplayer.clear()
 	grass_tilemaplayer.clear()
 	numbers_tilemaplayer.clear()
 	mines_tilemaplayer.clear()
 
-func generate_mines():
+func generate_mines() -> void:
 	for i in range(get_parent().total_mines):
 		var mine_pos = Vector2i(randi_range(0, COLS - 1), randi_range(2, ROWS - 1))
 		
@@ -127,7 +127,7 @@ func generate_mines():
 		#add mine to tilemaplayer
 		mines_tilemaplayer.set_cell(mine_pos, tile_id, mine_atlas)
 
-func generate_numbers():
+func generate_numbers() -> void:
 	for i in get_empty_cells():
 		var mine_count : int = 0
 		for j in get_all_surround_cells(i):
@@ -137,13 +137,13 @@ func generate_numbers():
 		if mine_count > 0:
 				numbers_tilemaplayer.set_cell(i, tile_id, number_atlas[mine_count - 1])
 
-func generate_grass():
+func generate_grass() -> void:
 	for y in range(ROWS):
 		for x in range(COLS):
 			var toggle = ((x + y) % 2)
 			grass_tilemaplayer.set_cell(Vector2i(x, y + 2), tile_id, Vector2i(3 - toggle, 0))
 
-func get_empty_cells():
+func get_empty_cells() -> Array:
 	var empty_cells := []
 	for y in range(ROWS):
 		for x in range(COLS):
@@ -152,7 +152,7 @@ func get_empty_cells():
 				empty_cells.append(Vector2i(x, y+2))
 	return empty_cells
 
-func get_all_surround_cells(center_cell):
+func get_all_surround_cells(center_cell) -> Array:
 	var surrounding_cells := []
 	var target_cell
 	for y in range(3):
@@ -166,14 +166,14 @@ func get_all_surround_cells(center_cell):
 					surrounding_cells.append(target_cell)
 	return surrounding_cells
 
-func is_mine(pos):
+func is_mine(pos) -> bool:
 	return mines_tilemaplayer.get_cell_source_id(pos) != -1
 
-func is_grass(pos):
+func is_grass(pos) -> bool:
 	return grass_tilemaplayer.get_cell_source_id(pos) != -1
 
-func is_flag(pos):
+func is_flag(pos) -> bool:
 	return flags_tilemaplayer.get_cell_source_id(pos) != -1
 	
-func is_number(pos):
+func is_number(pos) -> bool:
 	return numbers_tilemaplayer.get_cell_source_id(pos) != -1
