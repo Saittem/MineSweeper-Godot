6f7c97c97a97a19d3b074e91ea810365ed4edc4a
Saittem <TEOLF@email.cz>
2025-07-07 22:36:46 +0200

    Added global script for global variables

---
+++ minesweeper/global.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var difficulty : String = "hard"
+
+# mine count consts
+const MINE_COUNT_EASY : int = 20
+const MINE_COUNT_MEDIUM : int = 45
+const MINE_COUNT_HARD : int = 80
+
+# rows and cols for difficulties
+const ROWS_EASY : int = 12
+const COLS_EASY : int = 12
+
+const ROWS_MEDIUM : int = 17
+const COLS_MEDIUM : int = 17
+
+const ROWS_HARD : int = 24
+const COLS_HARD : int = 24
